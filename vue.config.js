42f77d59bea4cd28f4d0c6ef7b60bba90e5ad14a
Gerard March Moy <56315258+Gerard01mm@users.noreply.github.com>
2023-12-10 23:38:25 +0100

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,7 @@
 module.exports = {
     publicPath: 'http://localhost:8080',
-    outputDir: '../static/dist',
-    indexPath: '../../templates/_base_vue.html',
+    //outputDir: '../static/dist',
+    //indexPath: '../../templates/_base_vue.html',
 
     configureWebpack: {
         devServer: {
