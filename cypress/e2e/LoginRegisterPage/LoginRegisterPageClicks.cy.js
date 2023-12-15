b8914970586914581bada88f18c677a2bb51578a
IvanMartinezVilla <ivanmv2000@gmail.com>
2023-12-15 10:30:54 +0100

    Cypress Tests Pages

---
+++ cypress/e2e/LoginRegisterPage/LoginRegisterPageClicks.cy.js
@@ -0,0 +1,34 @@
+describe('Banner Click Tests', () => {
+    beforeEach(() => {
+        cy.visit('https://recipesharingfrontend.onrender.com')
+        cy.get(".user-image").should('be.visible').click()
+        cy.contains("Log In").should('be.visible').click()
+        cy.url().should('eq', 'https://recipesharingfrontend.onrender.com/loginRegister');
+    })
+
+    it("Home Page User Image Pop-Up is Correct", () => {
+        cy.get(".user-image").should('be.visible').click()
+    });
+
+    it("Home Page Home Page Button is Correct", () => {
+        cy.get(".button-image-logo").should('be.visible').click()
+    });
+})
+
+describe('Change container click Test', () => {
+    beforeEach(() => {
+        cy.visit('https://recipesharingfrontend.onrender.com')
+        cy.get(".user-image").should('be.visible').click()
+        cy.contains("Log In").should('be.visible').click()
+        cy.url().should('eq', 'https://recipesharingfrontend.onrender.com/loginRegister');
+    })
+
+    it("SignUp is marked correctly", () => {
+        cy.get('#signUp').should('be.visible').click()
+    });
+
+    it("Login is marked correctly", () => {
+        cy.get('#signUp').should('be.visible').click()
+        cy.get('#signIn').should('be.visible').click()
+    })
+})
